9ee26a5f0b4780af2277b077b19e2a44026a0814
fantasia85 <541799565@qq.com>
2020-02-23 15:42:11 +0800

    first

---
+++ Project1/Project1/source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello" << std::endl;
+	std::cout << " World" << std::endl;
+	std::cout << "!" << std::endl;
+	return 0;
+}
