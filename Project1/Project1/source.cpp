b98c9fbeb25057a9d20dc8edaa7d79ffed89870c
fantasia85 <541799565@qq.com>
2020-02-25 11:45:55 +0800

    test1

--- Project1/Project1/source.cpp
+++ Project1/Project1/source.cpp
@@ -2,12 +2,12 @@
 
 int main()
 {
-	std::cout << "hello_1_2_5" << std::endl;
+	std::cout << "hello_1_2_9_1" << std::endl;
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Hello" << std::endl;
 	std::cout << " World" << std::endl;
 	std::cout << "!" << std::endl;
 	std::cout << "helloworld!" << std::endl;
-	std::cout << "Hello_1_1_1" << std::endl;
+	std::cout << "Hello_1_1_5" << std::endl;
 	return 0;
 }
