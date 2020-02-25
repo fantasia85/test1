baf8d0a697f89c8b7930f91659fd38b44cf96905
fantasia85 <541799565@qq.com>
2020-02-25 11:44:27 +0800

    clone

--- Project1/Project1/source.cpp
+++ Project1/Project1/source.cpp
@@ -2,12 +2,12 @@
 
 int main()
 {
-	std::cout << "hello_1_2_5" << std::endl;
+	std::cout << "hello_1_2_4" << std::endl;
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Hello" << std::endl;
 	std::cout << " World" << std::endl;
 	std::cout << "!" << std::endl;
 	std::cout << "helloworld!" << std::endl;
-	std::cout << "Hello_1_1" << std::endl;
+	std::cout << "Hello_1_6" << std::endl;
 	return 0;
 }
